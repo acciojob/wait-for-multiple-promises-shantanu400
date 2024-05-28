1b9fee24b297480de416df88bddea836413f1b30
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-05-28 20:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function myPromise(name,age){
+	return new promise((res,rej)=>{
+		setTimeout(()=>{
+			if(age>18){
+				resolve(
+					alert("Welcome,",{name},". you can vote");
+				)
+				
+			}
+			else{
+				reject({result: "Oh sorry",{name},". You aren't old enough."})
+			}
+		},4000);
+	});
+	myPromise().then((data)=>{
+		
+	}).catch((err)=>{
+		alert(err)
+	}
+}
