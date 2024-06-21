215c2b83a67ff0fffd7926318c4e4e8b22dec10c
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-21 18:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,52 +1,46 @@
-let PromiseArray=[
-	new Promise((resolve,reject)=>{
-		let startTime=Date.now();
-		setTimeout(()=>{
-			let endTime=Date.now();
-			resolve({message:"Promise1 resolved",timetaken:endTime-startTime});
-		},Math.random()*3000);
-	}),
-	new Promise((resolve,reject)=>{
-		let startTime=Date.now();
-		setTimeout(()=>{
-			let endTime=Date.now();
-			resolve({message:"Promise2 resolved",timetaken:endTime-startTime});
-		},Math.random()*3000);
-	}),
-	new Promise((resolve,reject)=>{
-		let startTime=Date.now();
-				setTimeout(()=>{
-					let endTime=Date.now();
-					resolve({message:"Promise3 resolves",timetaken:endTime-startTime});
-				},Math.random()*3000);
-			
-	})
-]
-let totalTime=0;
-let var=document.getElementById('tableis');
-var.innerHTML=``;
-Promise.all(PromiseArray).then((datas)=>{
+let PromiseArray = [
+  new Promise((resolve, reject) => {
+    let startTime = Date.now();
+    setTimeout(() => {
+      let endTime = Date.now();
+      resolve({message: "Promise1 resolved", timeTaken: endTime - startTime});
+    }, Math.random() * 3000);
+  }),
+  new Promise((resolve, reject) => {
+    let startTime = Date.now();
+    setTimeout(() => {
+      let endTime = Date.now();
+      resolve({message: "Promise2 resolved", timeTaken: endTime - startTime});
+    }, Math.random() * 3000);
+  }),
+  new Promise((resolve, reject) => {
+    let startTime = Date.now();
+    setTimeout(() => {
+      let endTime = Date.now();
+      resolve({message: "Promise3 resolved", timeTaken: endTime - startTime});
+    }, Math.random() * 3000);
+  })
+];
 
-	datas.forEach((data)=>{
-		let var=document.getElementById('tableis');
-		totalTime+=data.timetaken;
-		var.innerHTML=`
-		<td>${data.message}</td>
-		<td>${data.timetaken}</td>
-	`
-	});
-	var.innerHTML+=`
-		<tr>
-		<td>Total</td>
-		<td>${totalTime}</td>
-		</tr>
-	`
-
-}).catch((error)=>{
-	console.log(`Error: ${error}`)
+let totalTime = 0;
+let table = document.getElementById('tableis');
+table.innerHTML = '';
+Promise.all(PromiseArray).then((datas) => {
+  datas.forEach((data) => {
+    totalTime += data.timeTaken;
+    table.innerHTML += `
+      <tr>
+        <td>${data.message}</td>
+        <td>${data.timeTaken}</td>
+      </tr>
+    `;
+  });
+  table.innerHTML += `
+    <tr>
+      <td>Total</td>
+      <td>${totalTime}</td>
+    </tr>
+  `;
+}).catch((error) => {
+  console.log(`Error: ${error}`);
 });
-
-
-	
-	
-		
