55e9ab2d0588019f176ad2dc6c263b57f9357f40
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-21 18:18:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-let promiseArray=[
-	new promise((resolve,reject)=>{
+let PromiseArray=[
+	new Promise((resolve,reject)=>{
 		let startTime=Date.now();
 		setTimeout(()=>{
 			let endTime=Date.now();
-			resolve({message:"promise1 resolved",timetaken:endTime-startTime}),Math.random()*3000)
-					   }
+			resolve({message:"Promise1 resolved",timetaken:endTime-startTime});
+		},Math.random()*3000);
 	}),
-	new promise((resolve,reject)=>{
+	new Promise((resolve,reject)=>{
 		let startTime=Date.now();
-		setTimeout(()=>
-			{let endTime=Date.now();
-			resolve({message:"promise2 resolved",timetaken:endTime-startTime}),Math.random()*3000)}
+		setTimeout(()=>{
+			let endTime=Date.now();
+			resolve({message:"Promise2 resolved",timetaken:endTime-startTime});
+		},Math.random()*3000);
 	}),
-	new promise((resolve,reject)=>{
+	new Promise((resolve,reject)=>{
 		let startTime=Date.now();
-				setTimeout(()=>
-					{let endTime=Date.now();
-					resolve({message:"promise3 resolves",timetaken:endTime-startTime}),Maths.random()*3000)}
-			})
+				setTimeout(()=>{
+					let endTime=Date.now();
+					resolve({message:"Promise3 resolves",timetaken:endTime-startTime});
+				},Math.random()*3000);
+			
 	})
 ]
 let totalTime=0;
-promise.all(promiseArray).then((datas)=>{
+let var=document.getElementById('tableis');
+var.innerHTML=``;
+Promise.all(PromiseArray).then((datas)=>{
 
 	datas.forEach((data)=>{
-		let var=document.getElementById('tableis')
-		var.innerHTML=``;
+		let var=document.getElementById('tableis');
 		totalTime+=data.timetaken;
 		var.innerHTML=`
 		<td>${data.message}</td>
 		<td>${data.timetaken}</td>
 	`
 	});
-
-}).catch((error)=>{
-	console.log(`Error: ${error}`)
-})
-
-let var=document.getElementById('tableis')
 	var.innerHTML+=`
 		<tr>
 		<td>Total</td>
 		<td>${totalTime}</td>
 		</tr>
 	`
+
+}).catch((error)=>{
+	console.log(`Error: ${error}`)
+});
+
+
+	
 	
 		
