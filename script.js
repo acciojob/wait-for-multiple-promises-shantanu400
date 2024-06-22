bd78fc0d902e8e988ab07560aca02dd1954760f3
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-22 17:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ let PromiseArray = [
     setTimeout(() => {
       let endTime = Date.now();
       resolve({message: "Promise 1", timeTaken: (endTime - startTime)/1000});
-    }, Math.floor(Math.random()*3000)+1000);
+    }, Math.floor(Math.random()*2000)+1000);
   }),
   new Promise((resolve, reject) => {
     let startTime = Date.now();
     setTimeout(() => {
       let endTime = Date.now();
       resolve({message: "Promise 2", timeTaken: (endTime - startTime)/1000});
-    }, Math.floor(Math.random()*3000)+1000);
+    }, Math.floor(Math.random()*2000)+1000);
   }),
   new Promise((resolve, reject) => {
     let startTime = Date.now();
     setTimeout(() => {
       let endTime = Date.now();
       resolve({message: "Promise 3", timeTaken: (endTime - startTime)/1000});
-    }, Math.floor(Math.random()*3000)+1000);
+    }, Math.floor(Math.random()*2000)+1000);
   })
 ];
 
