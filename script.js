16c33a9c8ef41496dcea217d3977d9e906f97a93
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-22 17:37:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,32 +4,32 @@ let PromiseArray = [
     setTimeout(() => {
       let endTime = Date.now();
       resolve({message: "Promise1 resolved", timeTaken: endTime - startTime});
-    }, Math.floor((Math.random()*(3-1)+1) *1000);
+    }, Math.floor((Math.random()*1000));
   }),
   new Promise((resolve, reject) => {
     let startTime = Date.now();
     setTimeout(() => {
       let endTime = Date.now();
       resolve({message: "Promise2 resolved", timeTaken: endTime - startTime});
-    }, Math.floor((Math.random()*(3-1)+1) *1000);
+    }, Math.floor((Math.random()*1000));
   }),
   new Promise((resolve, reject) => {
     let startTime = Date.now();
     setTimeout(() => {
       let endTime = Date.now();
       resolve({message: "Promise3 resolved", timeTaken: endTime - startTime});
-    }, Math.floor((Math.random()*(3-1)+1) *1000);
+    }, Math.floor((Math.random()*1000));
   })
 ];
 
 let totalTime = 0;
-let table = document.querySelector('#output');
+let table = document.getElementById('output');
 table.innerHTML = ``;
 Promise.all(PromiseArray).then((datas) => {
   datas.forEach((data) => {
     totalTime += data.timeTaken;
     table.innerHTML += `
-      <tr id="loading">
+      <tr >
         <td>${data.message}</td>
         <td>${data.timeTaken}</td>
       </tr>
